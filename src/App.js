900b08ac6e44eddad3db5b63be4f0d47e3813518
diana2341 <dianaponce2341@gmail.com>
2021-04-22 12:48:36 -0600

    lazy

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import {BrowserRouter as Router} from 'react-router-dom'
 import {Route, Switch} from 'react-router-dom'
 import Home from './components/Home'
 import Contact from './components/Contact'
-import Projects from './components/Projects'
+// import Projects from './components/Projects'
 import About from './components/About'
 import  Navbar  from './components/Navbar';
 import  Footer  from './components/Footer';
+import React, { Suspense,lazy } from 'react';
+const Projects = lazy(() => import('./components/Projects'));
 
 
 function App() {
@@ -15,12 +17,15 @@ function App() {
     <div className='App'>
       <Navbar/>
       <Router>
+       <Suspense fallback={<div>Loading...</div>}>
+
         <Switch>
           <Route exact path='/' render={()=><Home/>}/>
           <Route exact path='/about' render={()=><About/>}/>
           <Route exact path='/contact' render={()=><Contact/>}/>
-          <Route exact path='/projects' render={()=><Projects/>}/>
-        </Switch>  
+          <Route exact path='/projects' render={ ()=> <Projects/> }/>
+        </Switch> 
+        </Suspense>
       </Router>
       <Footer/>
     </div>

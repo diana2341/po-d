28a9fa20f2ee605ff655f17c525ce655ebdb9526
diana2341 <dianaponce2341@gmail.com>
2021-04-22 13:41:31 -0600

    emailjs

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {Card,Accordion,Button,Form,Container,Row,Col} from 'react-bootstrap'
+import {Card,Accordion,Button,Form,Container,Row,Col,Alert} from 'react-bootstrap'
 import {HiOutlineMail} from'react-icons/hi'
 import {HiOutlineMailOpen} from'react-icons/hi'
 import {SiLinkedin} from'react-icons/si'
@@ -8,11 +8,41 @@ import {SiGithub} from'react-icons/si'
 
 
 
-export default function Contact(){
-    const [closed, setClosed] = useState(false);
+export default class Contact extends React.Component{
+    state={
+        feedback: '',
+        name: '', 
+        email: '' ,
+        closed:false,
+        show:false
+    }
+    handleChange=(event) =>{
+    const{name,value}=event.target
+    this.setState({[name]:value})
+    }
 
-    return(
+    handleSubmit= (e)=>{
+      e.preventDefault();
+      const templateId = "template_OSnwlhmv";;
+      this.setState({name:'',feedback:'',email:''})
+      this.sendFeedback(templateId, {message_html: this.state.feedback, from_name: this.state.name, reply_to: this.state.email})
+      console.log('sent')
+      this.setState({show:true})
+
+    }
+
+    sendFeedback= (templateId, variables) =>{
+      window.emailjs.send(
+        'gmail', templateId,
+        variables).then(res => {
+            console.log('Email successfully sent!')
+        })
+          .catch(err => console.error('Oh well, you failed. Here some thoughts on the error that occured:', err))
+    }
+    render(){return(
         <Container className='col-contact'><br/>
+        
+
             <span className='bye'><h2>Thank You For Visiting My Portfolio ! <span className='wave'>👋</span></h2><br/>
 <h3>I hope you enjoyed looking around Im always learning new technologies and making new projects. You can always come back to check out new content!
 You can contact me through email by clicking the mail icon Or you contact me through LinkedIn and checkout my GitHub.</h3></span><br/><br/>
@@ -30,23 +60,27 @@ You can contact me through email by clicking the mail icon Or you contact me thr
                     <Card className='card-l'>
                         <Card.Header className='accor-card'>
                         <Accordion.Toggle actvive ={false}className='b-link'as={Button} variant="link" eventKey="0">
-                        <h1 id={`${closed?'':'mail'}`}onClick={()=>setClosed(!closed)}>{closed?< HiOutlineMailOpen/>:<HiOutlineMail />}</h1> 
+                        <h1 id={`${this.state.closed?'':'mail'}`}onClick={()=>this.setState({closed:!this.state.closed})}>{this.state.closed?< HiOutlineMailOpen/>:<HiOutlineMail />}</h1> 
                         </Accordion.Toggle>
                         </Card.Header>
                         <Accordion.Collapse eventKey="0">
                         <Card.Body>
-                        <Form className='form'>
+                        <Form className='form' onSubmit={this.handleSubmit}>
                         <Form.Group controlId="formBasicName">
-                        <Form.Control type="name" placeholder="Name" />
+                        <Form.Control name='name'type="name" placeholder="Name"     required
+                onChange={this.handleChange}
+                value={this.state.name}/>
                     
                     </Form.Group>
                     <Form.Group controlId="formBasicEmail">
-                        <Form.Control type="email" placeholder="Email" />
+                        <Form.Control required name='email'type="email" placeholder="Email"   onChange={this.handleChange}
+                value={this.state.email}/>
                     
                     </Form.Group>
 
                     <Form.Group controlId="formBasicMessage.textarea">
-                        <Form.Control as="textarea" rows={3} placeholder="Message" />
+                        <Form.Control name='feedback'required as="textarea" rows={3} placeholder="Message" onChange={this.handleChange}
+                value={this.state.feedback}/>
                     </Form.Group>
                     
                     <Button id='sub-btn'variant="outline-light" type="submit">
@@ -58,7 +92,14 @@ You can contact me through email by clicking the mail icon Or you contact me thr
                     </Card>
                     </Accordion>
                 </Col>
-            </Row>
+            </Row><br/><br/>
+            {this.state.show&&
+      <Alert variant="light" onClose={() => this.setState({show:false})} dismissible>
+        <Alert.Heading>Message sent!</Alert.Heading>
+      </Alert>
+    
+  }
         </Container>
     )
+    }
 } 

2ae96e3f9b2cfcc5b4c18a967a2705f17b3bb9b7
diana2341 <59631643+diana2341@users.noreply.github.com>
2022-02-13 12:35:40 -0500

    grammar fixes

--- src/components/info.js
+++ src/components/info.js
@@ -31,7 +31,7 @@ const info = [
         main:taco,
         name: 'Taco-Haven',
         tools:'Boostrap, CSS, HTML, React',
-        description:'This web application was made to show a layout of a Mexican Restaurant example site. It was made to show my design skills and ideas',
+        description:'This web application was made to show a layout of a Mexican Restaurant example site and to show my design skills and ideas',
         gitLink:'https://github.com/diana2341/tac-p',
         deploy:'https://taco-haven-restaurant.netlify.app/'
 
@@ -65,7 +65,7 @@ const info = [
         main:shopmart,
         name: 'Shop-mart',
         tools:'Ruby Rails, CSS, HTML, React, Redux',
-        description:' This Web Application was made to better understand my knowledge of redux. It is A  ecommerce site for clothing, Shoes, and accessories for women, men and children powered with  React + Redux ',
+        description:' This Web Application was made to better understand my knowledge of redux. It is a ecommerce site for clothing, shoes, and accessories for women, men and children ',
         gitLink:'https://github.com/diana2341/shopMart-front',
         // deploy:'https://shop-mart-us.netlify.app/'
 
@@ -78,7 +78,7 @@ const info = [
       tools:'React.js, Ruby Rails API, CSS, HTML',
       libraries:'React slide-show-image, React Simple Maps',
       video:"https://www.youtube.com/embed/xTjHOjz2xGA",
-      description:"An educational site  that raises awareness about several animal species endangered due to poaching. Reasons for poaching are explained to the user. They can also view recent news about the animal and are able to join a community to discuss  the topic with others.",
+      description:"An educational site  that raises awareness about several animal species endangered due to poaching. Reasons for poaching are explained to the user. They can also view recent news about the animal and are able to join a community to discuss  the topic with others",
       gitLink:'https://github.com/diana2341/why-poaching-front',
       youtube:'https://www.youtube.com/watch?v=xTjHOjz2xGA&t=13s',      
     },
@@ -89,7 +89,7 @@ const info = [
         name: 'Travel Rails',
         tools:'Ruby Rails, CSS, HTML, Bulma.io',
         video:"https://streamable.com/e/ai4u1c",
-        description:'A site for hosting homes as stays for people on vacation and where homes, apartments, or rooms can be booked by people traveling. Users can become hosts and post their homes or find a home.',
+        description:'A site for hosting homes for people on vacation and where homes, apartments, or rooms can be booked by people traveling. Users can become hosts and post their homes or find a home',
         gitLink:'https://github.com/diana2341/My_Travel_rails',
         youtube:'https://streamable.com/ai4u1c',
     },
@@ -100,7 +100,7 @@ const info = [
         name: 'Illucid Beats',
         tools:'Vanilla Javascript, CSS, HTML, Ruby Rails API',
         video:"https://www.youtube.com/embed/pOq3eoo0Mjs",
-        description:'A sound machine used to reduce stress, increase concentration or enable the ability to sleep better with ambient noises. Users can make a mix of ambient noises and listen to others creations.',
+        description:'A sound machine used to reduce stress, increase concentration or enable the ability to sleep better with ambient noises. Users can make a mix of ambient noises and listen to others creations',
         gitLink:'https://github.com/diana2341/illucid-beats-front',
         youtube:'https://www.youtube.com/watch?v=pOq3eoo0Mjs',
         // deploy:'https://illucid-beats.netlify.app/'
@@ -125,7 +125,7 @@ const info = [
         name: 'Sugar Lips',
         tools:'React.js, Ruby Rails API, CSS,HTML',
         video:"https://www.youtube.com/embed/J1_E9mqnAM8",
-        description:'An ecommerce site for a bakery that sells cupcakes, where  users can order any amount of these treats to be delivered to their homes!',
+        description:'An ecommerce site for a bakery that sells cupcakes, where  users can order any amount of these treats to be delivered to their homes',
         gitLink:'https://github.com/diana2341/mod4-sugarlips_cupcakes-client',
         youtube:'https://www.youtube.com/watch?v=J1_E9mqnAM8&t=2s',
     }

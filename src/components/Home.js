e3fc6616f3040d1768ed1d8bb0d4a5a73d57ace1
diana2341 <59631643+diana2341@users.noreply.github.com>
2022-06-27 22:50:06 -0400

    2022 rrsm

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,45 +1,44 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Button } from 'react-bootstrap';
-import Me from '../imgs/developer6.js';
+import React, { useState, useEffect } from "react";
+import { Container, Button } from "react-bootstrap";
+import Me from "../imgs/developer6.js";
 
-import { AiOutlineMail } from 'react-icons/ai';
-import { FaLinkedinIn } from 'react-icons/fa';
-import { AiOutlineGithub } from 'react-icons/ai';
-import { AiOutlineInstagram } from 'react-icons/ai';
-import { AiOutlinePauseCircle } from 'react-icons/ai';
+import { AiOutlineMail } from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+import { AiOutlineGithub } from "react-icons/ai";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { AiOutlinePauseCircle } from "react-icons/ai";
 
-import { AiOutlinePlayCircle } from 'react-icons/ai';
-import pausedBG from '../imgs/me.svg';
-import playdBG from '../imgs/bg.svg';
+import { AiOutlinePlayCircle } from "react-icons/ai";
+import pausedBG from "../imgs/me.svg";
+import playdBG from "../imgs/bg.svg";
 
-import { Jumbotron, Row, Col } from 'react-bootstrap';
-import { SiCss3 } from 'react-icons/si';
-import { SiHtml5 } from 'react-icons/si';
-import { SiReact } from 'react-icons/si';
-import { SiRedux } from 'react-icons/si';
-import { SiMysql } from 'react-icons/si';
-import { SiJavascript } from 'react-icons/si';
+import { Jumbotron, Row, Col } from "react-bootstrap";
+import { SiCss3 } from "react-icons/si";
+import { SiHtml5 } from "react-icons/si";
+import { SiReact } from "react-icons/si";
+import { SiRedux } from "react-icons/si";
+import { SiMysql } from "react-icons/si";
+import { SiJavascript } from "react-icons/si";
 
-import { SiGithub } from 'react-icons/si';
-import { DiRuby } from 'react-icons/di';
-import { SiPostgresql } from 'react-icons/si';
-import { SiBootstrap } from 'react-icons/si';
-import { SiVuedotjs } from 'react-icons/si';
-import { SiShopify } from 'react-icons/si';
-import { SiNodedotjs } from 'react-icons/si';
-import { SiTypescript } from 'react-icons/si';
-import { SiNextdotjs } from 'react-icons/si';
-import { SiMaterialui } from 'react-icons/si';
-import { SiMongodb } from 'react-icons/si';
-import { SiPostman } from 'react-icons/si';
-import { SiFigma } from 'react-icons/si';
+import { SiGithub } from "react-icons/si";
+import { DiRuby } from "react-icons/di";
+import { SiPostgresql } from "react-icons/si";
+import { SiBootstrap } from "react-icons/si";
+import { SiVuedotjs } from "react-icons/si";
+import { SiShopify } from "react-icons/si";
+import { SiNodedotjs } from "react-icons/si";
+import { SiTypescript } from "react-icons/si";
+import { SiNextdotjs } from "react-icons/si";
+import { SiMaterialui } from "react-icons/si";
+import { SiMongodb } from "react-icons/si";
+import { SiPostman } from "react-icons/si";
+import { SiFigma } from "react-icons/si";
 
-import info from './info';
+import info from "./info";
 
-import Typewriter from 'typewriter-effect';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
+import Typewriter from "typewriter-effect";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function Home() {
 	const [project, setProject] = useState();
@@ -48,23 +47,23 @@ export default function Home() {
 		AOS.init({
 			offset: 200,
 			duration: 600,
-			easing: 'ease-in-sine',
+			easing: "ease-in-sine",
 			delay: 50,
-			once: true
+			once: true,
 		});
 	});
 	const [paused, setPaused] = useState(false);
 
 	const pauseUpdate = () => {
 		if (paused) {
-			document.querySelector('.App').style.backgroundImage = `url(${pausedBG})`;
+			document.querySelector(".App").style.backgroundImage = `url(${pausedBG})`;
 			setPaused(false);
-			console.log('paused');
+			console.log("paused");
 		} else {
-			document.querySelector('.App').style.backgroundImage = `url(${playdBG})`;
+			document.querySelector(".App").style.backgroundImage = `url(${playdBG})`;
 
 			setPaused(true);
-			console.log('play');
+			console.log("play");
 		}
 	};
 	// let returnGreeting = () => {
@@ -80,27 +79,22 @@ export default function Home() {
 	// 	}
 	// };
 	const showPausebtn = () => {
-		document.querySelector('.pause-play').style.display = 'block';
-		document.querySelector('.placeholder').style.display = 'none';
-
+		document.querySelector(".pause-play").style.display = "block";
+		document.querySelector(".placeholder").style.display = "none";
 	};
 
 	return (
 		<>
 			<div className="container-main" id="intro">
-				
-				<div className='me-svg'>
+				<div className="me-svg">
 					<Me />
 				</div>
 
-				<div
-				className='text-wrapper'
-					
-				>
+				<div className="text-wrapper">
 					{/* <h1>{returnGreeting()} Visitor!</h1><br/> */}
 
-					<div className='main-text-container' >
-						<h1 className='notranslate'>
+					<div className="main-text-container">
+						<h1 className="notranslate">
 							{/* <Typewriter
 								options={{
 									strings: ['Hi, my name is <b>Diana!</b>'],
@@ -117,7 +111,7 @@ export default function Home() {
 								}}
 								onInit={(typewriter) => {
 									typewriter
-										.typeString('Hi, my name is <b>Diana!</b>')
+										.typeString("Hi, my name is <b>Diana!</b>")
 										.callFunction(() => {
 											setInterval(showPausebtn, 600);
 										})
@@ -126,33 +120,29 @@ export default function Home() {
 								}}
 							/>
 						</h1>
-						<div className='placeholder' style={{opacity:'0'}}>
-						{paused ? <AiOutlinePlayCircle /> : <AiOutlinePauseCircle />}
-
+						<div className="placeholder" style={{ opacity: "0" }}>
+							{paused ? <AiOutlinePlayCircle /> : <AiOutlinePauseCircle />}
 						</div>
 						<h2 className="pause-play" onClick={pauseUpdate}>
-							
 							{paused ? <AiOutlinePlayCircle /> : <AiOutlinePauseCircle />}
 						</h2>
 					</div>
 
 					<p>
-						I am a software engineer based in New York . I specialize in Frontend development 
-						and mobile development. As a software engineer, I strive to develop
-						efficient and innovative solutions in order to optimize a user's
-						experience and satisfaction. I enjoy learning new and upcoming
-						changes in technology to enhance my skill set. I am a person who is
-						well-organized, a problem solver, and an independent worker who pays
-						close attention to details. While I have worked with many
-						technologies my main tech stack is in React.js, React native, and
-						liquid!
+						I am a software engineer based in New York . I specialize in Frontend development and
+						mobile development. As a software engineer, I strive to develop efficient and innovative
+						solutions in order to optimize a user's experience and satisfaction. I enjoy learning
+						new and upcoming changes in technology to enhance my skill set. I am a person who is
+						well-organized, a problem solver, and an independent worker who pays close attention to
+						details. While I have worked with many technologies my main tech stack is in React.js,
+						React native, and liquid!
 					</p>
 					<div
 						style={{
-							display: 'flex',
-							flexDirection: 'row',
-							alignItems: 'center',
-							gap: '30px',
+							display: "flex",
+							flexDirection: "row",
+							alignItems: "center",
+							gap: "30px",
 						}}
 					>
 						<h1 className="btn-lnk">
@@ -192,13 +182,13 @@ export default function Home() {
 						<Row md={5}>
 							<Col>
 								<h1>
-									<SiCss3 />{' '}
+									<SiCss3 />{" "}
 								</h1>
 								CSS
 							</Col>
 							<Col>
 								<h1>
-									<SiHtml5 />{' '}
+									<SiHtml5 />{" "}
 								</h1>
 								HTML5
 							</Col>
@@ -265,7 +255,7 @@ export default function Home() {
 							<Col>
 								<h1>
 									<SiNodedotjs />
-								</h1>{' '}
+								</h1>{" "}
 								Nodejs
 							</Col>
 							<Col>
@@ -317,17 +307,16 @@ export default function Home() {
 					<p>
 						<Button
 							className="link-btn"
-							onClick={() =>
-								document.getElementById('project')?.scrollIntoView(true)
-							}
+							onClick={() => document.getElementById("project")?.scrollIntoView(true)}
 							variant="outline-light"
 						>
 							View Projects
-						</Button>{' '}
+						</Button>{" "}
 						<Button
 							className="link-btn"
-							href="https://drive.google.com/file/d/1crVMB1ABjJXDHwwVeTpfjhOwc_EzFYu1/view?usp=sharing"
+							href="https://drive.google.com/file/d/1S_cf6LC3jvYhP-PN50vionoevgG3sRKT/view?usp=sharing"
 							variant="outline-light"
+							target="_blank"
 						>
 							View Resume
 						</Button>
@@ -346,13 +335,8 @@ export default function Home() {
 							<div key={index} className="card" data-aos="zoom-in">
 								<div className="card-horizontal">
 									<a href={info.deploy ? info.deploy : info.youtube}>
-										<img
-											className="picturep"
-											variant="top"
-											alt="pic"
-											src={info.main}
-										/>
-									</a>{' '}
+										<img className="picturep" variant="top" alt="pic" src={info.main} />
+									</a>{" "}
 									{/* data-aos="fade-left" */}
 									<div className="body ">
 										<div>
@@ -364,37 +348,25 @@ export default function Home() {
 										<br />
 										<div className="external-l">
 											{info.gitLink ? (
-												<Button
-													size="sm"
-													href={info.gitLink}
-													variant="outline-light"
-												>
+												<Button size="sm" href={info.gitLink} variant="outline-light">
 													Source Code
 												</Button>
 											) : (
-												''
-											)}{' '}
+												""
+											)}{" "}
 											{info.youtube ? (
-												<Button
-													size="sm"
-													href={info.youtube}
-													variant="outline-light"
-												>
+												<Button size="sm" href={info.youtube} variant="outline-light">
 													Demo Video
 												</Button>
 											) : (
-												''
-											)}{' '}
+												""
+											)}{" "}
 											{info.deploy ? (
-												<Button
-													size="sm"
-													href={info.deploy}
-													variant="outline-light"
-												>
+												<Button size="sm" href={info.deploy} variant="outline-light">
 													Live Site
 												</Button>
 											) : (
-												''
+												""
 											)}
 										</div>
 									</div>
@@ -405,14 +377,13 @@ export default function Home() {
 				</div>
 				<div id="link">
 					<h4 id="con-me">
-						// Like what you see?{' '}
-						<a href="mailto: dianap2341@gmail.com"> Contact me!</a>
+						// Like what you see? <a href="mailto: dianap2341@gmail.com"> Contact me!</a>
 					</h4>
 					<br />
 					<br />
 					<br />
 				</div>
-				<br/>
+				<br />
 			</div>
 		</>
 	);

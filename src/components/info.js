9e39cb138dba35b73e1311219b7b35ddb986bf37
Diana Ponce <dianap2341@gmail.com>
2024-02-13 13:33:19 -0500

    project update pass 1

--- src/components/info.js
+++ src/components/info.js
@@ -8,7 +8,10 @@ import portfolio from '../imgs/portfolio.png'
 import construction from '../imgs/construction.png'
 import dwell from '../imgs/dwell.png'
 import hiphop from '../imgs/hiphop.png'
-import illucidv2 from '../imgs/illucidv2.png'
+import renovar from '../imgs/renovar.png'
+import illucidvv2 from '../imgs/illucidvv2.png'
+import hrhealthCare from '../imgs/hrhealthcare.png'
+import cliniclean from '../imgs/cliniclean.png'
 
 
 
@@ -19,7 +22,7 @@ const info = [
     {
         id:10,
         class:'illucid-v2',
-        main:illucidv2,
+        main:illucidvv2,
         name: 'Illucid beats V2',
         description:'Version 2 of Illucid Beats, A sound machine used to reduce stress, increase concentration or enable the ability to sleep better with ambient noises. Users can make a mix of ambient noises and listen to others creations',
         tools:' Vue, Vuex, MongoDB, CSS, HTML',
@@ -35,6 +38,36 @@ const info = [
         tools:' CSS, HTML, React',
         description:'Web application for a wine company',
         deploy:'https://www.wineclub.dwell.com/'
+
+    },
+    {
+        id:11,
+        class:'renovar',
+        main:renovar,
+        name: 'Renovar Wound Care',
+        tools:' Wordpress, Elementor, Google analytics',
+        description:' Designed and developed Web site for a pharmaceutical company. This product specializes in wound care products. Utilizing Elementor on WordPress, I crafted a user-friendly platform seamlessly integrating product information. Through the incorporation of Google Analytics, I provided valuable insights to the client',
+        deploy:'https://renovarwoundcare.com/'
+
+    },
+    {
+        id:12,
+        class:'hrhealthCare',
+        main:hrhealthCare,
+        name: 'HR HealthCare',
+        tools:' Wordpress, Elementor, Google analytics',
+        description:" Designed and developed a healthcare website for a pharmaceutical company aiming to modernize its online presence in the healthcare sector. Utilizing Elementor and WordPress, I crafted a user-friendly platform that seamlessly integrated product information. Incorporating Google Analytics provided valuable insights into user engagement. The result exceeded the client's expectations, delivering a fresh and engaging showcase for their pharmaceutical offerings",
+        deploy:'https://www.hrhealthcare.com/'
+
+    },
+    {
+        id:13,
+        class:'cliniclean',
+        main:cliniclean,
+        name: 'Cliniclean',
+        tools:' Wordpress, Elementor, Google analytics',
+        description:' I designed and developed a website for a pharmaceutical company focusing on antiseptic solution products. Leveraging Elementor on WordPress, I created a user-friendly platform that seamlessly integrates product information. Additionally, by incorporating Google Analytics, I offered valuable insights to the client',
+        deploy:'https://cliniclean.com/'
 
     },
     {
@@ -92,7 +125,7 @@ const info = [
       video:"https://www.youtube.com/embed/xTjHOjz2xGA",
       description:"An educational site  that raises awareness about several animal species endangered due to poaching. Reasons for poaching are explained to the user. They can also view recent news about the animal and are able to join a community to discuss  the topic with others",
     //   gitLink:'https://github.com/diana2341/why-poaching-front',
-      youtube:'https://www.youtube.com/watch?v=xTjHOjz2xGA&t=13s',      
+    //   youtube:'https://www.youtube.com/watch?v=xTjHOjz2xGA&t=13s',      
     },
     {
         id:4,
@@ -103,18 +136,18 @@ const info = [
         video:"https://streamable.com/e/ai4u1c",
         description:'A site for hosting homes for people on vacation and where homes, apartments, or rooms can be booked by people traveling. Users can become hosts and post their homes or find a home',
         // gitLink:'https://github.com/diana2341/My_Travel_rails',
-        youtube:'https://streamable.com/ai4u1c',
+        // youtube:'https://streamable.com/ai4u1c',
     },
     {
         id:2,
         class:'illucid',
         main:illucid,
-        name: 'Illucid Beats',
+        name: 'Illucid Beats (V1)',
         tools:'Vanilla Javascript, CSS, HTML, Ruby Rails API',
         video:"https://www.youtube.com/embed/pOq3eoo0Mjs",
         description:'A sound machine used to reduce stress, increase concentration or enable the ability to sleep better with ambient noises. Users can make a mix of ambient noises and listen to others creations',
         // gitLink:'https://github.com/diana2341/illucid-beats-front',
-        youtube:'https://www.youtube.com/watch?v=pOq3eoo0Mjs',
+        // youtube:'https://www.youtube.com/watch?v=pOq3eoo0Mjs',
         // deploy:'https://illucid-beats.netlify.app/'
     },
     {
@@ -125,22 +158,22 @@ const info = [
         tools:'React.js, Bootstrap, CSS, HTML',
         // video:"https://www.youtube.com/embed/J1_E9mqnAM8",
         description:'My First portfolio site',
-        gitLink:'https://github.com/diana2341/portfolio',
+        // gitLink:'https://github.com/diana2341/portfolio',
         // youtube:'https://www.youtube.com/watch?v=J1_E9mqnAM8&t=2s',
         // deploy:'https://dianaponcedev.netlify.app'
 
     },
-    {
-        id:3,
-        class:'sugar',
-        main:cupcake,
-        name: 'Sugar Lips',
-        tools:'React.js, Ruby Rails API, CSS,HTML',
-        video:"https://www.youtube.com/embed/J1_E9mqnAM8",
-        description:'An ecommerce site for a bakery that sells cupcakes, where  users can order any amount of these treats to be delivered to their homes',
-        // gitLink:'https://github.com/diana2341/mod4-sugarlips_cupcakes-client',
-        youtube:'https://www.youtube.com/watch?v=J1_E9mqnAM8&t=2s',
-    }
+    // {
+    //     id:3,
+    //     class:'sugar',
+    //     main:cupcake,
+    //     name: 'Sugar Lips',
+    //     tools:'React.js, Ruby Rails API, CSS,HTML',
+    //     video:"https://www.youtube.com/embed/J1_E9mqnAM8",
+    //     description:'An ecommerce site for a bakery that sells cupcakes, where  users can order any amount of these treats to be delivered to their homes',
+    //     // gitLink:'https://github.com/diana2341/mod4-sugarlips_cupcakes-client',
+    //     youtube:'https://www.youtube.com/watch?v=J1_E9mqnAM8&t=2s',
+    // }
 
   ];
   

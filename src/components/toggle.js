70062a9b85682396e05b56ae8f147d1ed6ca9d98
samanthaponce5 <dianap2341@gmail.com>
2024-03-19 00:30:12 -0400

    focus outline

--- src/components/toggle.js
+++ src/components/toggle.js
@@ -1,23 +1,42 @@
-import React from 'react';
+import React from "react";
 
-function Toggle({light, lightDark}) {
-   
+function Toggle({ light, lightDark }) {
   return (
-   
-  <label className='toggle-contain' aria-label="Toggle for night mode or dark mode">
-    <input className='toggle-checkbox' type='checkbox' onChange={lightDark}checked={light} aria-hidden={true}></input>
-    <div className='toggle-slot'>
-      <div className='sun-icon-wrapper'>
-        <div className="iconify sun-icon" data-icon="feather-sun" data-inline="false"></div>
+    <label
+      className="toggle-contain"
+      aria-label={`Toggle for light or dark mode`}
+      tabIndex="0"
+      htmlFor="toggle-mode"
+      onKeyDown={(e) => (e.key === "Enter" ? lightDark() : null)}
+    >
+      <input
+        className="toggle-checkbox"
+        type="checkbox"
+        onChange={lightDark}
+        checked={light}
+        aria-hidden={true}
+        tabIndex="-1"
+        id="toggle-mode"
+      ></input>
+      <div className="toggle-slot">
+        <div className="sun-icon-wrapper">
+          <div
+            className="iconify sun-icon"
+            data-icon="feather-sun"
+            data-inline="false"
+          ></div>
+        </div>
+        <div className="toggle-button"></div>
+        <div className="moon-icon-wrapper">
+          <div
+            className="iconify moon-icon"
+            data-icon="feather-moon"
+            data-inline="false"
+          ></div>
+        </div>
       </div>
-      <div className='toggle-button'></div>
-      <div className='moon-icon-wrapper'>
-        <div className="iconify moon-icon" data-icon="feather-moon" data-inline="false"></div>
-      </div>
-    </div>
-  </label>
-  
-  )
+    </label>
+  );
 }
 
 export default Toggle;
